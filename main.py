3edc23da4f3c4873d7726c5d87b8658b0e56c5a7
josephgleal <jleal2018@gmail.com>
2021-05-24 13:02:54 -0400

    only need to test on Pi Zero for now

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import board
 import os
 import sys
 import subprocess
+import time
 
 # TODO
-# impliment temperature warnings
-# impliment timing so readings are less frequent
 # error handling for when first reading fails
 # test on Pi zero instead of Pi 4
+# if Pi zero has issues, use a for loop inside the while loop to impliment an average, while also still utilizing the sleep function
+# impliment error logging
 
 
 #######################start up ######################
@@ -40,6 +41,11 @@ Celcius = 0
 temperature = 0
 humidity = 0
 
+highHumidity = False
+lowHumidity = False
+lowTemperature = False
+highTemperature = False
+
 ###########functions#####################
 READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
 @blynk.handle_event('read V0')
@@ -57,6 +63,7 @@ def read_virtual_pin_handler(pin):
 
 # Retrieves temperature data and optionally pushes to supergraph on pin V2
 def getTemperature():
+    global temperature
     try:
         Celcius = dht_device.temperature
         temperature = (Celcius * 9/5)+32
@@ -69,6 +76,7 @@ def getTemperature():
 
 # Retrieves humidity data and optionally pushes to supergraph on pin V3
 def getHumidity():
+    global humidity
     try:
         humidity = dht_device.humidity
         blynk.virtual_write(3, humidity)
@@ -76,14 +84,52 @@ def getHumidity():
     except RuntimeError:
         print("error with humidity reading")
         
-def main():
-    while True:
-        blynk.run()  # runs blynk read/listen handlers, and establishes connection
-        temperature = getTemperature()
-        humidity = getHumidity()
-        print("T",temperature, "H:",humidity)
+def warnHumidity(low, high):
+    lowError = "Humidity below " + str(low) + " detected"
+    highError = "Humidity above " + str(high) + "% detected"
+    global lowHumidity
+    global highHumidity
+    if humidity > high and highHumidity == False:
+        blynk.notify(highError)
+        highHumidity = True
+    elif humidity < low and lowHumidity == False:
+        blynk.notify(lowError)
+        lowHumidity = True
+    elif humidity < high and humidity > low:
+        highHumidity = False
+        lowHumidity = False
+        
+def warnTemperature(low, high):
+    lowError = "Temperature below " + str(low) + "F detected"
+    highError = "Temperature above " + str(high) + "F detected"
+    global lowTemperature
+    global highTemperature
+    try:
+        if temperature > high and highTemperature == False:
+            blynk.notify(highError)
+            highTemperature = True
+        elif temperature < low and lowTemperature == False:
+            blynk.notify(lowError)
+            lowTemperature = True
+        elif temperature > low and temperature < high:
+            highTemperature = False
+            lowTemperature = False
+    except TypeError:
+        print("TypeError in warnTemperature, check previous temperature value")
+
+        
+
+while True:
+    blynk.run()  # runs blynk read/listen handlers, and establishes connection
+    temperature = getTemperature()
+    humidity = getHumidity()
+    print("T",temperature, "H:",humidity)
+    warnHumidity(30,65)
+    warnTemperature(72,75)
+    time.sleep(2)
+    
 
 
 ################main################
-main()
+
     
